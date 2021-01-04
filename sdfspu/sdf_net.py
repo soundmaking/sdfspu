164af2602d07b18c45a8182cd5e9638628c7e165
Samuel Freeman <soundmaking@merfasmean.com>
2021-01-04 12:24:24 +0000

    sdf_net cite source

--- sdfspu/sdf_net.py
+++ sdfspu/sdf_net.py
@@ -1,5 +1,8 @@
 import socket
+
+
 def get_ip():
+    # https://stackoverflow.com/a/28950776/9471339
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
         # doesn't even have to be reachable
