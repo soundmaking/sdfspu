e120bad3b1468402ad55e5f675185ccdad2b3eed
Samuel Freeman <soundmaking@merfasmean.com>
2021-01-04 00:36:05 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sdfspu',
+      version='0.1.001',
+      description='sdfspu: Some Python Utilities, by sdf',
+      url='https://github.com/soundmaking/sdfspu',
+      author='Samuel Freeman',
+      author_email='soundmaking@merfasmean.com',
+      license='MIT',
+      packages=['sdfspu'],
+      install_requires=['requests'],
+      )
